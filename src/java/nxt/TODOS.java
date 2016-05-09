52e55853d77bab89f251c2bcce037098ced8637c
Worker <you@example.com>
2016-05-09 08:43:18 +0200

    added dummy todo class for quick communication

---
+++ src/java/nxt/TODOS.java
@@ -0,0 +1,10 @@
+package nxt;
+
+public class TODOS {
+/**
+ * 
+ * I have removed a function that was checking for the transparent pforging block 8, please check if it was important. Do't recall the name
+ * 
+ */
+}
+
