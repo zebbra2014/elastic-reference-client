ea693d7dd567a35fb023fb218f6bfaa468e41f3b
Bahndamm <root@bahndamm>
2016-05-23 12:28:50 +0200

    final sync

---
+++ src/java/nxt/http/GetAccountWorkEfficiencyPlot.java
@@ -0,0 +1,88 @@
+package nxt.http;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import nxt.Account;
+import nxt.Nxt;
+import nxt.NxtException;
+import nxt.Transaction;
+import nxt.db.DbIterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONStreamAware;
+
+
+
+public final class GetAccountWorkEfficiencyPlot extends APIServlet.APIRequestHandler {
+
+	static final GetAccountWorkEfficiencyPlot instance = new GetAccountWorkEfficiencyPlot();
+
+	private GetAccountWorkEfficiencyPlot() {
+		super(new APITag[] { APITag.ACCOUNTS, APITag.WC }, "account",
+				"timestamp", "type", "subtype", "firstIndex", "lastIndex",
+				"numberOfConfirmations", "withMessage");
+	}
+
+	@SuppressWarnings("unchecked")
+	JSONArray dateValuePair(long timestamp, double d){
+		JSONArray nullValue = new JSONArray();
+		nullValue.add(timestamp);
+		nullValue.add(d);
+		return nullValue;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	JSONStreamAware processRequest(FakeServletRequest req) throws NxtException {
+		JSONObject response = new JSONObject();
+
+		
+
+		long workId;
+		try {
+			workId = Long.parseLong(req.getParameter("workId"));
+		} catch (NumberFormatException e) {
+			return response;
+		}
+		
+		long common_timestamp = (new Date()).getTime();
+		
+		JSONArray computation_power = getComputationPlot(workId, common_timestamp);
+		JSONArray solution_rate = getSolutionPlot(workId, common_timestamp);
+		
+		
+		for(int i=0;i<100;i++){
+			common_timestamp = common_timestamp - 20;
+			computation_power.add(dateValuePair(common_timestamp, 1337.0 - i*10));
+			solution_rate.add(dateValuePair(common_timestamp, 1337.0 - i*5));
+		}
+		
+		
+		response.put("computation_power", computation_power);
+		response.put("solution_rate", solution_rate);
+		
+		// Delay to test Javascript Loading Indicator
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+
+		}
+		
+		return response;
+
+	}
+
+	private JSONArray getSolutionPlot(long workId, long common_timestamp) {
+		JSONArray solution_rate = new JSONArray();
+		
+		return solution_rate;
+	}
+
+	private JSONArray getComputationPlot(long workId, long common_timestamp) {
+		JSONArray computation_power = new JSONArray();
+		
+		return computation_power;
+	}
+
+}
